d7d576396e71b8848bf7620453d0ff0c1627ca3a
jordanlb-724 <jordan@rhinorails.com>
2024-07-22 11:53:41 -0400

    7-22-2024

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 import { defineConfig } from 'astro/config';
-import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 import lit from "@astrojs/lit";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://odyssey-theme.pages.dev/',
-    // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-    sitemap: true,
-    // Generate sitemap (set to "false" to disable)
-    integrations: [sitemap(), mdx(), lit(), icon(), tailwind(), react()] // Add renderers to the config
+  site: 'https://odyssey-theme.pages.dev/',
+  // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  sitemap: true,
+  // Generate sitemap (set to "false" to disable)
+  integrations: [mdx(), lit(), icon(), tailwind(), react(), sitemap()] // Add renderers to the config
 });
---
+++ src/components/theme-provider.jsx
@@ -0,0 +1,8 @@
+// import React from 'react';
+// import { ThemeProvider } from '@material-tailwind/react';
+
+// const CustomThemeProvider = ({ children }) => {
+//     return <ThemeProvider>{children}</ThemeProvider>;
+// };
+
+// export default CustomThemeProvider;
